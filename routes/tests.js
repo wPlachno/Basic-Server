3a0ec0ad93225b4b4c9292b96887a08a3b2961cd
wPlachno <wjplachno@gmail.com>
2022-10-12 07:57:04 -0700

    10/12/22 Fixed some gitignore and the time string not being readable (wjplachno)

--- routes/tests.js
+++ routes/tests.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require('../db');
 router.get("/", (request, response) => {
-  db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at $
-{Date.now()}')`)
+  db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at `+ (new Date(Date.now())).toUTCString() +`')`)
     .then( _ => db.any(`SELECT * FROM test_table`) )
     .then( results => response.json( results ) )
     .catch( error => {
